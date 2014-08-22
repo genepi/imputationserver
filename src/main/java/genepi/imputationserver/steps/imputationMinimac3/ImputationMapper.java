37368633f4a9713c67267ae819ceeaadaecbd5a4
Sebastian Schoenherr <c703456@uibk.ac.at>
2014-08-22 07:26:43 +0000

    adding new imputation step for minimac3.

---
+++ src/main/java/genepi/imputationserver/steps/imputationMinimac3/ImputationMapper.java
@@ -0,0 +1,228 @@
+package genepi.imputationserver.steps.imputationMinimac3;
+
+import genepi.hadoop.CacheStore;
+import genepi.hadoop.HdfsUtil;
+import genepi.hadoop.ParameterStore;
+import genepi.hadoop.PreferenceStore;
+import genepi.hadoop.log.Log;
+import genepi.imputationserver.steps.imputation.sort.ChunkKey;
+import genepi.imputationserver.steps.imputation.sort.ChunkValue;
+import genepi.imputationserver.steps.vcf.VcfChunk;
+import genepi.imputationserver.steps.vcf.VcfChunkOutput;
+import genepi.io.FileUtil;
+import genepi.io.bed.BedUtil;
+import genepi.io.text.LineReader;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class ImputationMapper extends
+		Mapper<LongWritable, Text, ChunkKey, ChunkValue> {
+
+	private ImputationPipeline pipeline;
+
+	public String folder;
+
+	private String pattern;
+
+	private String phasing;
+
+	private String output;
+
+	private String refFilename = "";
+
+	private Log log;
+
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+
+		log = new Log(context);
+
+		// get parameters
+		ParameterStore parameters = new ParameterStore(context);
+		pattern = parameters.get(ImputationJob.REF_PANEL_PATTERN);
+		output = parameters.get(ImputationJob.OUTPUT);
+		phasing = parameters.get(ImputationJob.PHASING);
+		String hdfsPath = parameters.get(ImputationJob.REF_PANEL_HDFS);
+		String referencePanel = FileUtil.getFilename(hdfsPath);
+		String minimacBin = parameters.get(ImputationJob.MINIMAC_BIN);
+		
+		
+		// get cached files
+		CacheStore cache = new CacheStore(context.getConfiguration());
+		refFilename = cache.getArchive(referencePanel);
+		String minimacCommand = cache.getFile(minimacBin);
+		String hapiUrCommand = cache.getFile("hapi-ur");
+		String vcfCookerCommand = cache.getFile("vcfCooker");
+		String vcf2HapCommand = cache.getFile("vcf2hap");
+		String shapeItCommand = cache.getFile("shapeit");
+
+		// create temp directory
+		PreferenceStore store = new PreferenceStore(context.getConfiguration());
+		folder = store.getString("minimac.tmp");
+		folder = FileUtil.path(folder, context.getTaskAttemptID().toString());
+		FileUtil.createDirectory(folder);
+
+		// load config
+		int minimacWindow = Integer.parseInt(store.getString("minimac.window"));
+		int phasingWindow = Integer.parseInt(store.getString("phasing.window"));
+
+		// config pipeline
+		pipeline = new ImputationPipeline();
+		pipeline.setMinimacCommand(minimacCommand);
+		pipeline.setHapiUrCommand(hapiUrCommand);
+		pipeline.setVcfCookerCommand(vcfCookerCommand);
+		pipeline.setVcf2HapCommand(vcf2HapCommand);
+		pipeline.setShapeItCommand(shapeItCommand);
+		pipeline.setMinimacWindow(minimacWindow);
+		pipeline.setPhasingWindow(phasingWindow);
+
+	}
+
+	@Override
+	protected void cleanup(Context context) throws IOException,
+			InterruptedException {
+
+		// delete temp directory
+		log.close();
+		FileUtil.deleteDirectory(folder);
+
+	}
+
+	public void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+
+		if (value.toString() == null || value.toString().isEmpty()) {
+			return;
+		}
+
+		VcfChunk chunk = new VcfChunk(value.toString());
+		VcfChunkOutput outputChunk = new VcfChunkOutput(chunk, folder);
+		
+		System.out.println("name is: " + outputChunk.getVcfFilename());
+
+		HdfsUtil.get(chunk.getVcfFilename(), outputChunk.getVcfFilename());
+
+		log.info("Starting pipeline for chunk " + chunk + "...");
+
+		String chrFilename = pattern
+				.replaceAll("\\$chr", chunk.getChromosome());
+		
+		String refPanelFilename = FileUtil.path(refFilename, chrFilename);
+
+		if (!new File(refPanelFilename).exists()) {
+			log.stop("ReferencePanel '" + refPanelFilename + "' not found.", "");
+		}
+
+		pipeline.init();
+		pipeline.setReferencePanel(refPanelFilename);
+
+		if (chunk.isPhased()) {
+
+		// imputation with VCF files
+			if (!chunk.getChromosome().equals("23")
+					&& !chunk.getChromosome().equals("X")) {
+
+				boolean successful = pipeline.imputeMach(chunk, outputChunk);
+				if (successful) {
+					log.info("  Minimac successful.");
+				} else {
+					log.stop("  Minimac failed", "");
+					return;
+				}
+			}
+
+		} else {
+
+			System.out.println("vcf lines: "
+					+ FileUtil.getLineCount(outputChunk.getVcfFilename()));
+
+			// convert vcf to bim/bed/fam
+			boolean successful = pipeline.vcfToBed(outputChunk);
+			if (successful) {
+				log.info("  vcfCooker successful.");
+			} else {
+				log.stop("  vcfCooker failed", "");
+				return;
+			}
+
+			// ignore small chunks
+			int noSnps = pipeline.getNoSnps(chunk, outputChunk);
+			if (noSnps <= 2) {
+				log.info("  Chunk " + chunk + " has only " + noSnps
+						+ " markers. Ignore it.");
+				return;
+			} else {
+				log.info("  Before imputation: " + noSnps + " SNPs");
+			}
+
+			// remove parents
+			BedUtil.removeParents(outputChunk.getFamFilename());
+
+			// phasing
+			if (!phasing.equals("shapeit")) {
+
+				// hapiur
+				successful = pipeline.phaseWithHapiUr(chunk, outputChunk);
+				if (successful) {
+					log.info("  HapiUR successful.");
+				} else {
+					log.stop("  HapiUR failed", "");
+					return;
+				}
+
+			} else {
+
+				// shapeit
+				successful = pipeline.phaseWithShapeIt(chunk, outputChunk);
+				if (successful) {
+					log.info("  ShapeIt successful.");
+				} else {
+					log.stop("  ShapeIt failed", "");
+					return;
+				}
+
+			}
+
+			// imputation
+			if (!chunk.getChromosome().equals("23")
+					&& !chunk.getChromosome().equals("X")) {
+
+				successful = pipeline.imputeShapeIt(chunk, outputChunk);
+				if (successful) {
+					log.info("  Minimac successful.");
+				} else {
+
+					String stdOut = FileUtil.readFileAsString(outputChunk
+							.getPrefix() + ".minimac.out");
+					String stdErr = FileUtil.readFileAsString(outputChunk
+							.getPrefix() + ".minimac.err");
+
+					log.stop("  Minimac failed", "StdOut:\n" + stdOut
+							+ "\nStdErr:\n" + stdErr);
+					return;
+				}
+			}
+
+		}
+
+		// fix window bug in minimac
+		int[] indices = pipeline.fixInfoFile(chunk, outputChunk);
+		log.info("  Postprocessing successful.");
+
+		// store info file
+		HdfsUtil.put(outputChunk.getInfoFixedFilename(),
+				HdfsUtil.path(output, chunk.getChromosome(), chunk + ".info"),
+				context.getConfiguration());
+		
+		// store vcf file
+				HdfsUtil.put(outputChunk.getVcfOutFilename(),
+						HdfsUtil.path(output, chunk.getChromosome(), chunk + ".dose.vcf"),
+						context.getConfiguration());
+
+	}
+}
---
+++ src/main/java/genepi/imputationserver/steps/imputationMinimac3/ImputationPipeline.java
@@ -0,0 +1,240 @@
+package genepi.imputationserver.steps.imputationMinimac3;
+
+import genepi.hadoop.command.Command;
+import genepi.imputationserver.steps.vcf.VcfChunk;
+import genepi.imputationserver.steps.vcf.VcfChunkOutput;
+import genepi.io.FileUtil;
+import genepi.io.plink.MapFileReader;
+import genepi.io.plink.Snp;
+import genepi.io.text.LineReader;
+import genepi.io.text.LineWriter;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ImputationPipeline {
+
+	private String minimacCommand;
+	private String hapiUrCommand;
+	private String shapeItCommand;
+	private String vcfCookerCommand;
+	private String vcf2HapCommand;
+	private String refPanelFilename;
+	private int minimacWindow;
+	private int phasingWindow;
+
+	public void init() {
+
+	}
+
+	public void setMinimacCommand(String minimacCommand) {
+		this.minimacCommand = minimacCommand;
+	}
+
+	public void setHapiUrCommand(String hapiUrCommand) {
+		this.hapiUrCommand = hapiUrCommand;
+	}
+
+	public void setVcfCookerCommand(String vcfCookerCommand) {
+		this.vcfCookerCommand = vcfCookerCommand;
+	}
+
+	public void setVcf2HapCommand(String vcf2HapCommand) {
+		this.vcf2HapCommand = vcf2HapCommand;
+	}
+
+	public void setShapeItCommand(String shapeItCommand) {
+		this.shapeItCommand = shapeItCommand;
+	}
+
+	public void setMinimacWindow(int minimacWindow) {
+		this.minimacWindow = minimacWindow;
+	}
+
+	public void setPhasingWindow(int phasingWindow) {
+		this.phasingWindow = phasingWindow;
+	}
+
+	public void setReferencePanel(String refPanelFilename) {
+		this.refPanelFilename = refPanelFilename;
+	}
+
+	public boolean vcfToBed(VcfChunkOutput output) {
+
+		Command vcfCooker = new Command(vcfCookerCommand);
+		vcfCooker.setSilent(false);
+
+		vcfCooker.setParams("--in-vcf", output.getVcfFilename(), "--write-bed",
+				"--out", output.getPrefix());
+		vcfCooker.saveStdOut(output.getPrefix() + ".vcfcooker.out");
+		vcfCooker.saveStdErr(output.getPrefix() + ".vcfcooker.err");
+		return (vcfCooker.execute() == 0);
+
+	}
+
+	public boolean vcfToHap(VcfChunkOutput output) {
+		Command vcf2Hap = new Command(vcf2HapCommand);
+		vcf2Hap.setSilent(false);
+		vcf2Hap.setParams("--in-vcf", output.getVcfFilename(), "--out",
+				output.getPrefix());
+		vcf2Hap.saveStdOut(output.getPrefix() + ".vcf2hap.out");
+		vcf2Hap.saveStdErr(output.getPrefix() + ".vcf2hap.err");
+		return (vcf2Hap.execute() == 0);
+	}
+
+	public int getNoSnps(VcfChunk input, VcfChunkOutput output) {
+		MapFileReader reader = null;
+
+		try {
+			System.out.println(FileUtil.getLineCount(output.getBimFilename()));
+		} catch (IOException e1) {
+			System.out.println(e1.getMessage());
+			e1.printStackTrace();
+		}
+
+		int noSnps = 0;
+		try {
+			reader = new MapFileReader(output.getBimFilename());
+			while (reader.next()) {
+				Snp snp = reader.get();
+				System.out.println(snp.getPhysicalPosition());
+				if (snp.getPhysicalPosition() >= input.getStart()
+						&& snp.getPhysicalPosition() <= input.getEnd()) {
+					noSnps++;
+				}
+			}
+			reader.close();
+		} catch (Exception e) {
+			System.out.println("Error during snp count: " + e.getMessage());
+			e.printStackTrace();
+			return -1;
+		}
+		return noSnps;
+	}
+
+	public boolean phaseWithHapiUr(VcfChunk input, VcfChunkOutput output) {
+
+		// +/- 1 Mbases
+		int start = input.getStart() - phasingWindow;
+		if (start < 1) {
+			start = 1;
+		}
+
+		int end = input.getEnd() + phasingWindow;
+
+		Command hapiUr = new Command(hapiUrCommand);
+		hapiUr.setSilent(false);
+
+		hapiUr.setParams("-p", output.getPrefix(), "-w", "73", "-o",
+				output.getPrefix(), "-c", input.getChromosome(), "--start",
+				start + "", "--end", end + "", "--impute2");
+		hapiUr.saveStdOut(output.getPrefix() + ".hapiur.out");
+		hapiUr.saveStdErr(output.getPrefix() + ".hapiur.err");
+
+		return (hapiUr.execute() == 0);
+	}
+
+	public boolean phaseWithShapeIt(VcfChunk input, VcfChunkOutput output) {
+
+		Command shapeIt = new Command(shapeItCommand);
+		shapeIt.setSilent(false);
+
+		shapeIt.setParams("--input-bed", output.getBedFilename(),
+				output.getBimFilename(), output.getFamFilename(),
+				"--output-max", output.getPrefix(), "--input-from",
+				input.getStart() + "", "--input-to", input.getEnd() + "");
+		shapeIt.saveStdOut(output.getPrefix() + ".shapeit.out");
+		shapeIt.saveStdErr(output.getPrefix() + ".shapeit.err");
+
+		return (shapeIt.execute() == 0);
+	}
+
+	public boolean imputeShapeIt(VcfChunk input, VcfChunkOutput output)
+			throws InterruptedException, IOException {
+
+		if (!new File(refPanelFilename).exists()) {
+			throw new InterruptedException("ReferencePanel '"
+					+ refPanelFilename + "' not found.");
+		}
+
+		// mini-mac
+		Command minimac = new Command(minimacCommand);
+		minimac.setSilent(false);
+
+		minimac.setParams("--vcfReference", "--refHaps", refPanelFilename,
+				"--sample", output.getSampleFilename(), "--shape_haps",
+				output.getHapsFilename(), "--rounds", "5", "--vcfstart",
+				input.getStart() + "", "--vcfend", input.getEnd() + "",
+				"--vcfwindow", minimacWindow + "", "--vcfchr",
+				input.getChromosome(), "--prefix", output.getPrefix(), "--chr",
+				input.getChromosome());
+
+		minimac.saveStdOut(output.getPrefix() + ".minimac.out");
+		minimac.saveStdErr(output.getPrefix() + ".minimac.err");
+
+		return (minimac.execute() == 0);
+
+	}
+
+	public boolean imputeMach(VcfChunk input, VcfChunkOutput output)
+			throws InterruptedException, IOException {
+
+		// mini-mac
+		Command minimac = new Command(minimacCommand);
+		minimac.setSilent(false);
+
+		minimac.setParams("--vcfReference", "--refHaps", refPanelFilename,
+				 "--haps",
+				output.getVcfFilename(), "--rounds", "5", "--start",
+				input.getStart() + "", "--end", input.getEnd() + "",
+				"--window", minimacWindow + "", "--phased","--vcfOutput",
+				input.getChromosome(), "--prefix", output.getPrefix(), "--chr",
+				input.getChromosome());
+
+		minimac.saveStdOut(output.getPrefix() + ".minimac.out");
+		minimac.saveStdErr(output.getPrefix() + ".minimac.err");
+
+		return (minimac.execute() == 0);
+
+	}
+
+	public int[] fixInfoFile(VcfChunk input, VcfChunkOutput output)
+			throws IOException, InterruptedException {
+
+		// fix window bug in minimac
+
+		LineReader readerInfo = new LineReader(output.getInfoFilename());
+		LineWriter writerInfo = new LineWriter(output.getInfoFixedFilename());
+
+		readerInfo.next();
+		String header = readerInfo.get();
+		writerInfo.write(header);
+
+		int startIndex = Integer.MAX_VALUE;
+		int endIndex = 0;
+
+		int index = 0;
+		int snps = 0;
+		while (readerInfo.next()) {
+			String line = readerInfo.get();
+			String[] tiles = line.split("\t", 2);
+			int position = Integer.parseInt(tiles[0].split(":")[1]);
+			if (position > input.getStart() && position < input.getEnd()) {
+				startIndex = Math.min(startIndex, index);
+				endIndex = Math.max(endIndex, index);
+				writerInfo.write(line);
+				snps++;
+			}
+			index++;
+		}
+
+		// log.info("After imputation: " + snps + " SNPs");
+
+		readerInfo.close();
+		writerInfo.close();
+
+		return new int[] { startIndex, endIndex };
+
+	}
+
+}
