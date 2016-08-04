ea6ca3e10e40a9a21941ee47d51aa8ebd81b4633
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2016-08-04 13:44:32 +0200

    adding init.sh for docker

---
+++ files/minimac/init.sh
@@ -0,0 +1,11 @@
+
+
+wget cloudgene.uibk.ac.at/downloads/hapmap.tar.gz -O /opt/cloudgene/tmp/hapmap.tar.gz
+cd/opt/cloudgene.tmp; tar xvfz hapmap.tar.gz
+su - cloudgene
+hadoop fs -mkdir ref-panels
+hadoop fs -mkdir ref-panels/hapmap_r22.eagle
+hadoop fs -put /opt/cloudgene/tmp/hapmap/bcfs/*.bcf* ref-panels/hapmap_r22.eagle/.
+hadoop fs -put /opt/cloudgene/tmp/hapmap/panel/hapmap_r22.CEU.hg19.tgz  ref-panels/.
+hadoop fs -put /opt/cloudgene/tmp/hapmap/panel/hapmap_r22.CEU.hg19.legends.tgz  ref-panels/.
+hadoop fs -put /opt/cloudgene/tmp/hapmap/map/genetic_map_hg19_withX.txt.gz  ref-panels/.
