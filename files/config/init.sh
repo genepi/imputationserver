5d50fd5580d8d4afe5fdb59cbf0960baede2af5a
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2016-08-16 10:26:48 +0200

    adding app to settings.yaml

--- files/config/init.sh
+++ files/config/init.sh
@@ -5,9 +5,9 @@ cd /opt/cloudgene/tmp; tar xvfz hapmap.tar.gz
 sudo -u cloudgene hadoop fs -mkdir ref-panels
 sudo -u cloudgene hadoop fs -mkdir ref-panels/hapmap_r22.eagle
 sudo -u cloudgene hadoop fs -put /opt/cloudgene/tmp/bcfs/*.bcf* ref-panels/hapmap_r22.eagle/.
-sudo -u cloudgene hadoop fs -put /opt/cloudgene/tmp/panel/hapmap_r22.CEU.hg19.tgz  ref-panels/.
-sudo -u cloudgene hadoop fs -put /opt/cloudgene/tmp/panel/hapmap_r22.CEU.hg19.legends.tgz  ref-panels/.
-sudo -u cloudgene hadoop fs -put /opt/cloudgene/tmp/map/genetic_map_hg19_withX.txt.gz  ref-panels/.
+sudo -u cloudgene hadoop fs -put /opt/cloudgene/tmp/panel/hapmap_r22.CEU.hg19.tgz ref-panels/.
+sudo -u cloudgene hadoop fs -put /opt/cloudgene/tmp/panel/hapmap_r22.CEU.hg19.legends.tgz ref-panels/.
+sudo -u cloudgene hadoop fs -put /opt/cloudgene/tmp/map/genetic_map_hg19_withX.txt.gz ref-panels/.
 
 # Connect Michigan Imputation Server with Cloudgene
 sudo cp /opt/cloudgene/applications/imputationserver/files/config/settings.yaml -R /opt/cloudgene/config/settings.yaml
