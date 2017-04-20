2409c68f493842a0eb78215eddf65c9eab11f23d
lukfor <lukas.forer@i-med.ac.at>
2017-04-20 17:05:33 +0200

    Improved VCFFileReader to support complex formats.

--- src/main/java/genepi/imputationserver/steps/fastqc/FastVCFFileReader.java
+++ src/main/java/genepi/imputationserver/steps/fastqc/FastVCFFileReader.java
@@ -17,27 +17,11 @@ public class FastVCFFileReader extends LineReader {
 
 	private int samplesCount = 0;
 
-	private int samplesInLineCount = 0;
-
-	private int noCallCount = 0;
-
-	private int hetCount = 0;
-
-	private int homRefCount = 0;
-
-	private int homVarCount = 0;
-
-	private int i = 0;
-
-	private int countR = 0;
-
-	private int countV = 0;
-
-	private int countNo = 0;
-		
 	private MinimalVariantContext variantContext;
 
 	private List<String> header = new Vector<>();
+
+	private VCFLineParser parser;
 
 	public FastVCFFileReader(String vcfFilename) throws IOException {
 
@@ -49,6 +33,8 @@ public class FastVCFFileReader extends LineReader {
 		samplesCount = samples.size();
 		variantContext = new MinimalVariantContext(samplesCount);
 		reader.close();
+
+		parser = new VCFLineParser(samplesCount);
 
 	}
 
@@ -74,88 +60,15 @@ public class FastVCFFileReader extends LineReader {
 		// not a header line
 		if (line.charAt(0) != '#') {
 
-			String tiles[] = line.split("\t", 10);
+			variantContext = parser.parseLine(line);
 
-			if (tiles.length < 3) {
-				throw new IOException("The provided VCF file is not tab-delimited");
-			}
-
-			String chromosome = tiles[0];
-			int position = Integer.parseInt(tiles[1]);
-			String ref = tiles[3];
-			String alt = tiles[4];
-
-			i = 0;
-			countR = 0;
-			countV = 0;
-
-			homRefCount = 0;
-			homVarCount = 0;
-			hetCount = 0;
-			noCallCount = 0;
-			
-			samplesInLineCount = 0;
-			while (i < tiles[9].length()) {
-				countR = 0;
-				countV = 0;
-				countNo = 0;
-				// count genotpyes for one sample
-				while (i < tiles[9].length() && tiles[9].charAt(i) != '\t') {
-					if (tiles[9].charAt(i) == '1') {
-						countV++;
-					} else if (tiles[9].charAt(i) == '0') {
-						countR++;
-					} else if (tiles[9].charAt(i) == '.') {
-						countNo++;
-					}
-					i++;
-				}
-				// check if it is hom or het
-				if (countR == 2 || (countR == 1 && countV == 0)) {
-					homRefCount++;
-				} else if (countV == 2 || (countV == 1 && countR == 0)) {
-					homVarCount++;
-				} else
-
-				if (countV == 1 && countR == 1) {
-					hetCount++;
-				}
-				i++;
-				
-				if (countNo==2 || (countNo==1 &&countV == 0 && countR == 0)){
-					noCallCount++;
-					variantContext.setCalled(samplesInLineCount, false);
-				}else{
-					variantContext.setCalled(samplesInLineCount, true);
-				}
-							
-				samplesInLineCount++;
-			}
-
-			if (samplesInLineCount != samplesCount) {
+			if (variantContext.getNSamples() != samplesCount) {
 				throw new IOException("Line " + getLineNumber() + ": different number of samples.");
 			}
 
-			// update variant context
-			variantContext.setContig(chromosome);
-			variantContext.setStart(position);
-			variantContext.setReferenceAllele(ref);
-			variantContext.setAlternateAllele(alt);
-			variantContext.setHetCount(hetCount);
-			variantContext.setHomRefCount(homRefCount);
-			variantContext.setHomVarCount(homVarCount);
-			variantContext.setNoCallCount(noCallCount);
-			variantContext.setNSamples(samplesInLineCount);
-			variantContext.setRawLine(line);
-			
-			if (!tiles[6].equals("PASS") && !tiles[6].equals(".")){
-				variantContext.setFilters(tiles[6]);
-			}else{
-				variantContext.setFilters(null);
-			}
-			
 			snpsCount++;
-		}else{
+
+		} else {
 			header.add(line);
 			next();
 		}
