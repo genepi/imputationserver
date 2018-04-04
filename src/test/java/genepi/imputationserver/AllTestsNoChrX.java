a9f4886e7ec6b30b37991a10645b4aec815762b5
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2018-04-04 14:21:21 +0200

    refactor tests

--- src/test/java/genepi/imputationserver/AllTestsNoChrX.java
+++ src/test/java/genepi/imputationserver/AllTestsNoChrX.java
@@ -6,7 +6,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import genepi.imputationserver.steps.FastQualityControlTest;
 import genepi.imputationserver.steps.ImputationMinimac3Test;
-import genepi.imputationserver.steps.ImputationMinimacChrXTest;
 import genepi.imputationserver.steps.ImputationPipelineMinimac3Test;
 import genepi.imputationserver.steps.InputValidationTest;
 import genepi.imputationserver.steps.fastqc.VCFLineParserTest;
@@ -14,7 +13,8 @@ import genepi.imputationserver.steps.util.FileMergerTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ InputValidationTest.class, VCFLineParserTest.class, ImputationMinimac3Test.class, FileMergerTest.class,
-	FastQualityControlTest.class, ImputationPipelineMinimac3Test.class, ImputationMinimacChrXTest.class})
-public class AllTests {
+		FastQualityControlTest.class, ImputationPipelineMinimac3Test.class })
+
+public class AllTestsNoChrX {
 
 }
