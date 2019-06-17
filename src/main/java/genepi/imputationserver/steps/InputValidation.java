2850faa186bdec60a23acf6400150c5d6bb2ab4b
lukfor <lukas.forer@i-med.ac.at>
2019-06-17 17:18:59 +0200

    Fix tabix setup command

--- src/main/java/genepi/imputationserver/steps/InputValidation.java
+++ src/main/java/genepi/imputationserver/steps/InputValidation.java
@@ -54,7 +54,7 @@ public class InputValidation extends WorkflowStep {
 	}
 
 	protected void setupTabix(String folder) {
-		//VcfFileUtil.setTabixBinary(FileUtil.path(folder, "bin", "tabix"));
+		VcfFileUtil.setTabixBinary(FileUtil.path(folder, "bin", "tabix"));
 	}
 
 	private boolean checkVcfFiles(WorkflowContext context) {
