1ea6403ae84a3b3e7eefe4c798390be670227330
seppinho <sebastian.schoenherr@i-md.ac.at>
2020-03-14 11:40:40 +0100

    Prepare release 1.2.8

--- src/main/java/genepi/imputationserver/steps/imputation/ImputationPipeline.java
+++ src/main/java/genepi/imputationserver/steps/imputation/ImputationPipeline.java
@@ -16,7 +16,7 @@ import groovy.text.SimpleTemplateEngine;
 
 public class ImputationPipeline {
 
-	public static final String PIPELINE_VERSION = "michigan-imputationserver-1.2.7";
+	public static final String PIPELINE_VERSION = "michigan-imputationserver-1.2.8";
 	
 	public static final String IMPUTATION_VERSION = "minimac4-1.0.2";
 
