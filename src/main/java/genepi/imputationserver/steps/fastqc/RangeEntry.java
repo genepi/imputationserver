fb326e8dc093561a58ce62a041b7d39439a7fe9e
seppinho <sebastian.schoenherr@i-md.ac.at>
2020-03-27 09:22:35 +0100

    Add missing resource

---
+++ src/main/java/genepi/imputationserver/steps/fastqc/RangeEntry.java
@@ -0,0 +1,32 @@
+package genepi.imputationserver.steps.fastqc;
+
+public class RangeEntry {
+
+	public String chromosome;
+	public int start;
+	public int end;
+
+	public String getChromosome() {
+		return chromosome;
+	}
+
+	public void setChromosome(String chromosome) {
+		this.chromosome = chromosome;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}
+}
