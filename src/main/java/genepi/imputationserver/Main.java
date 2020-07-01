c34db6732a77133c0da1ef650a507aeef4ccea39
Lukas Forer <lukas.forer@i-med.ac.at>
2020-07-01 11:14:47 +0200

    Add version to commandline-header

--- src/main/java/genepi/imputationserver/Main.java
+++ src/main/java/genepi/imputationserver/Main.java
@@ -3,6 +3,7 @@ package genepi.imputationserver;
 import java.lang.reflect.InvocationTargetException;
 
 import genepi.base.Toolbox;
+import genepi.imputationserver.steps.imputation.ImputationPipeline;
 import genepi.imputationserver.tools.LegendFileLiftOverTool;
 import genepi.imputationserver.tools.LegendFileTool;
 import genepi.imputationserver.tools.VcfLiftOverTool;
@@ -12,6 +13,12 @@ public class Main extends Toolbox {
 
 	public Main(String command, String[] args) {
 		super(command, args);
+		
+		System.out.println();
+		System.out.println(ImputationPipeline.PIPELINE_VERSION);
+		System.out.println("https://imputationserver.sph.umich.edu");
+		System.out.println("Lukas Forer, Sebastian Schoenherr and Christian Fuchsberger");
+		
 	}
 
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, SecurityException,
