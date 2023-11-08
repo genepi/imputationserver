1215b0d47488a15860f350607a9b2ad4b0caeadd
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2023-11-08 12:38:05 +0100

    Prepare release v1.8.0-beta1

--- src/main/java/genepi/imputationserver/steps/imputation/ImputationPipeline.java
+++ src/main/java/genepi/imputationserver/steps/imputation/ImputationPipeline.java
@@ -24,7 +24,7 @@ import lukfor.progress.tasks.Task;
 
 public class ImputationPipeline {
 
-	public static final String PIPELINE_VERSION = "michigan-imputationserver-1.7.4";
+	public static final String PIPELINE_VERSION = "michigan-imputationserver-1.8.0-beta1";
 
 	public static final String IMPUTATION_VERSION = "minimac4-1.0.2";
 
