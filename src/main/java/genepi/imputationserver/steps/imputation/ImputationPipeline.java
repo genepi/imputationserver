6326390688ea93b4dff8798c2b3565507e748d0f
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2023-11-08 14:22:34 +0100

    Update imputation engine log to v4.1.4

--- src/main/java/genepi/imputationserver/steps/imputation/ImputationPipeline.java
+++ src/main/java/genepi/imputationserver/steps/imputation/ImputationPipeline.java
@@ -26,7 +26,7 @@ public class ImputationPipeline {
 
 	public static final String PIPELINE_VERSION = "michigan-imputationserver-1.8.0-beta1";
 
-	public static final String IMPUTATION_VERSION = "minimac4-1.0.2";
+	public static final String IMPUTATION_VERSION = "minimac-v4.1.4";
 
 	public static final String BEAGLE_VERSION = "beagle.18May20.d20.jar";
 
